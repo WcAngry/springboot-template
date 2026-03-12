7048475e04b28764c15c0ce284fdee8e77f2b9b9
WcAngry <927724938@qq.com>
2026-03-12 19:35:51 +0800

    feat:增加Hello接口及相关截图

---
+++ week01/hello-sample/src/test/java/com/itheima/hellosample/HelloSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.hellosample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
